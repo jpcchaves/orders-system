96e559227fed76fc62cbd12b2cc28ba67d979c6d
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2023-01-03 22:33:58 -0300

    initital commit

---
+++ src/main/java/com/orderssytem/orderssystem/OrdersSystemApplication.java
@@ -0,0 +1,13 @@
+package com.orderssytem.orderssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderssytem/orderssystem/OrdersSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderssytem.orderssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
