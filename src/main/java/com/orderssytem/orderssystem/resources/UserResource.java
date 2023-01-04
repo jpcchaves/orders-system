1616cacde996dff1b654258913cdca9995e73022
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2023-01-03 22:53:01 -0300

    feat: create user entity and resource

---
+++ src/main/java/com/orderssytem/orderssystem/resources/UserResource.java
@@ -0,0 +1,15 @@
+package com.orderssytem.orderssystem.resources;
+
+import com.orderssytem.orderssystem.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+  public ResponseEntity<User> findAll() {
+
+  }
+}
